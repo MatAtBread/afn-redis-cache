d8ec750c82068064446ac80b321af069b5eb9ce5
Matthew W <matthew.woolf@mailonline.co.uk>
2016-10-07 16:06:00 +0100

    Update REDIS connection logic to fast-fail cache operations when the REDIS server is unabailable

--- index.js
+++ index.js
@@ -5,45 +5,84 @@
 var redis = require('redis') ;
 
 module.exports = function(config){
-    var client = redis.createClient(config.redis) ;
+    if (!config || !config.redis) {
+        throw new Error("REDIS configuration missing");
+    }
+
+    // Throw away any logging the app isn't capturing
+    config.log = config.log || function(){} ;
+
+    // Work out what params to connect to redis with
+    var redisArgs = [], redisOpts ; 
+    if (typeof config.redis === "string") { // A REDIS url
+        redisArgs = [config.redis,redisOpts = {}] ;
+    } else {
+        redisArgs = [redisOpts = config.redis] ;
+    }
     
+    // By default, fail connections quickly so caches fall back to local memory
+    if (!('enable_offline_queue' in redisOpts)) {
+        redisOpts.enable_offline_queue = false ;
+        redisOpts.connect_timeout = 1000 ;
+    }
+    // On failure, fail the command(s) instantly and try to re-establish a connection in 10 seconds
+    redisOpts.retry_strategy = redisOpts.retry_strategy || function (options) { 
+        config.log("afn-redis-cache retry",options) ;
+        setTimeout(connectRedis,10000) ;
+        return undefined ;
+    } ;
+        
+    var client ;
+    function connectRedis() {
+        if (client && client.connected)
+            return ;
+        client = redis.createClient.apply(redis,redisArgs) ;
+        client.on('error',function(err){
+            // redisOpts.retry_strategy()
+            // Just eat them up - the individual calls should handle the error
+            config.log("afn-redis-cache error",err) ;
+        }) ;
+    }
+    
+    connectRedis() ;
+
     return {
         hashEncoding:'base64',
         createCache: function(cacheID) {
             if (cacheID) cacheID += ":" ;
             else cacheID = "" ;
-            return {
+            var self ;
+            return self = {
                 get:async function(key) {
                     client.get(cacheID+key,function(err,reply){
                         if (err)
-                            async throw err ;
+                            async return undefined ;
                         try {
                             async return JSON.parse(reply) ;
                         } catch (ex) {
-                            async throw ex ;
+                            async return undefined ;
                         }
                     }) ;
                 },
                 set:async function(key,data,ttl) {
                     client.setex(cacheID+key, ttl?ttl/1000:(config.defaultTTL || 60), JSON.stringify(data), function(err,reply){
-                        if (err) async throw err ; ;
-                        async return reply ;
+                        async return self ;
                     }) ;
                 },
                 'delete':async function(key) {
                     client.del(cacheID+key, function(err,reply){
-                        if (err) async throw err ; ;
-                        async return reply ;
+                        if (err) async return false ;
+                        async return true ;
                     }) ;
                 },
                 keys:async function() {
                     client.keys(cacheID+":*",function(err,reply){
                         if (err)
-                            async throw err ;
+                            async return [] ;
                         try {
                             async return JSON.parse(reply) ;
                         } catch (ex) {
-                            async throw ex ;
+                            async return [] ;
                         }
                     }) ;
                 }
