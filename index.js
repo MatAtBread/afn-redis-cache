d7ac86dfb04aa58d64d1f749c52d0fab100fc293
Matthew W <matthew.woolf@mailonline.co.uk>
2017-02-02 12:37:46 +0000

    Make the redis cache implementation "Promise-aware"
    When asked to set contents that doesn't contain data, a marker is
    written to redis to indicate an operation is in progress. An attempt to
    get the cache entry whilst this is in place results in retries (upto a
    defined timeout) in an attempt prevent unnecessary cross cluster-node
    re-entrancy.

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 'use nodent-promise';
+'use strict';
 
 /* An afn memo cache based on REDIS for clustered services */
 
 var redis = require('redis') ;
 
 module.exports = function(config){
+    var inProgress = "@promise" ;
+    
     if (!config || !config.redis) {
         throw new Error("REDIS configuration missing");
     }
@@ -46,6 +49,16 @@ module.exports = function(config){
     
     connectRedis() ;
 
+    // How long results should remain valid for in the cache (default: 1 min)
+    if (!('defaultTTL' in config))
+        config.defaultTTL = 60 ;
+    
+    // How long the cache should wait for an async function to 
+    // return (and populate the cache) before claiming to be empty
+    // (default: 0 - don't wait, different servers in a cluster will re-enter the underlying memoized function)
+    if (!('asyncTimeOut' in config))
+        config.asyncTimeOut = 0 ;
+    
     return {
         hashEncoding:'base64',
         createCache: function(cacheID) {
@@ -54,18 +67,55 @@ module.exports = function(config){
             var self ;
             return self = {
                 get:async function(key) {
-                    client.get(cacheID+key,function(err,reply){
-                        if (err)
-                            async return undefined ;
-                        try {
-                            async return JSON.parse(reply) ;
-                        } catch (ex) {
-                            async return undefined ;
-                        }
-                    }) ;
+                    var retries = 0, delay = 25, total = 0 ;
+                    (function waiting(){
+                        client.get(cacheID+key,function(err,reply){
+                            if (err) {
+//config.log('afn-redis error   '+cacheID+key,err) ;                               
+                                async return undefined ;
+                            }
+
+                            if (reply===null) {
+//config.log('afn-redis miss    '+cacheID+key) ;                               
+                                async return null ;
+                            }
+                            try {
+                                if (reply === inProgress) {
+//config.log('afn-redis wait    '+cacheID+key) ;                               
+                                    // We're still in progress
+                                    retries += 1 ;
+                                    delay += retries ;
+                                    total += delay ;
+                                    
+                                    if (total > config.asyncTimeOut * 1000) {
+                                        // Timed out
+//config.log('afn-redis timeout '+cacheID+key) ;                               
+                                        async return undefined ;
+                                    } else {
+                                        setTimeout(waiting, delay) ;
+                                    }
+                                } else {
+//config.log('afn-redis hit     '+cacheID+key) ;                               
+                                    async return JSON.parse(reply) ;
+                                }
+                            } catch (ex) {
+//config.log('afn-redis except   '+cacheID+key) ;                               
+                                async return undefined ;
+                            }
+                        }) ;
+                    })() ;
                 },
                 set:async function(key,data,ttl) {
-                    client.setex(cacheID+key, ttl?ttl/1000:(config.defaultTTL || 60), JSON.stringify(data), function(err,reply){
+                    // Are we being asked to cache an unresolved promise with no data
+                    var serialized ;
+
+                    if (!('data' in data)) {
+                        serialized = inProgress ;
+                    } else {
+                        serialized = JSON.stringify(data) ;
+                    }
+                    
+                    client.setex(cacheID+key, ttl?ttl/1000:config.defaultTTL, serialized, function(err,reply){
                         async return self ;
                     }) ;
                 },
